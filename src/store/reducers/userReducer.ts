88f6623fe12c42bb4d566c1cc39c880e152b76d3
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-01 17:16:37 +0300

    описан и затипизирован userReducer

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,24 @@
+//взаимодействие со списком пользователей
+import { UserState, UserAction, UserActionTypes } from "../../types/user";
+
+const initialState: UserState = {
+    users: [],
+    loading: false, // флаг загрузки
+    error: null, // сообщение об ошибке или null
+};
+
+export const userReducer = (
+    state = initialState,
+    action: UserAction
+): UserState => {
+    switch (action.type) {
+        case UserActionTypes.FETCH_USERS:
+            return { users: [], loading: true, error: null };
+        case UserActionTypes.FETCH_USERS_SUCCESS:
+            return { users: action.payload, loading: false, error: null };
+        case UserActionTypes.FETCH_USERS_ERROR:
+            return { users: [], loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
