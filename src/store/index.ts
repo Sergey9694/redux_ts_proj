04f8bad3756eba3057eac58566d7ec48eeb6eab0
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-01 16:29:53 +0300

    init proj

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+//инициализация стора
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import thunk from "redux-thunk";
+
+export const store = createStore({}, applyMiddleware(thunk));
