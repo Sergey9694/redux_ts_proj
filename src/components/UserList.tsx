1cd4404989ff9f592ed508079525441f1f977434
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-16 18:03:40 +0300

    исользование экшн креэйтора для получения данных и отрисовка на странице

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,32 @@
+import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useEffect } from "react";
+import { fetchUsers } from "../store/action-creator/user";
 
 const UserList: React.FC = () => {
     // Достаем данные из состояния
     const { error, loading, users } = useTypedSelector((state) => state.users);
-    return <div></div>;
+    const dispatch = useDispatch();
+
+    //используем экшн креэйтор fetchUsers
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+    if (loading) {
+        return <h1>Идет загрузка...</h1>;
+    }
+    if (error) {
+        return <h1>{error}</h1>;
+    }
+
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user.id}>{user.name}</div>
+            ))}
+        </div>
+    );
 };
 
 export default UserList;
