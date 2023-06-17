6435ab790734c27390800c1805219610bbfbc940
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-17 13:45:07 +0300

    todoReducer добавлен  в комбайн

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
+import { todoReducer } from "./todoReducer";
 
 // комбайн для объединения редьюсеров
 export const rootReducer = combineReducers({
     users: userReducer,
+    todos: todoReducer,
 });
 
 //Получили тип Редьюсера
